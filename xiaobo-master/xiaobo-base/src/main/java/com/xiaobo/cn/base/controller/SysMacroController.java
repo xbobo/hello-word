ffa3fe6f96e245d908e03481dcad145843b6d97d
xb <603786093@qq.com>
2017-08-28 16:54:45 +0800

    日志清理

--- xiaobo-master/xiaobo-base/src/main/java/com/xiaobo/cn/base/controller/SysMacroController.java
+++ xiaobo-master/xiaobo-base/src/main/java/com/xiaobo/cn/base/controller/SysMacroController.java
@@ -16,10 +16,6 @@ import com.xiaobo.cn.common.entity.R;
 /**
  * 通用字典
  *
- * @author ZhouChenglin
- * @email yczclcn@163.com
- * @url www.chenlintech.com
- * @date 2017年8月15日 下午12:54:33
  */
 @RestController
 @RequestMapping("/sys/macro")
